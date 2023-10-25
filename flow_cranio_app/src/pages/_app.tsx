7eb1264a8cca7346ad73d1f6dc90b91ac6152462
Marco Meyer <marcomeyer@posteo.de>
2023-10-25 12:10:19 +0200

    setup: setup tailwind-elements with demo view

--- flow_cranio_app/src/pages/_app.tsx
+++ flow_cranio_app/src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+import "tw-elements/dist/css/tw-elements.min.css";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
